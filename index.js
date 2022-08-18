7e30160eaef266dfa94466dba31d5fa3e50aa02e
awadh <awadh.hassan@student.moringaschool.com>
2022-08-18 14:44:07 +0300

    assignment done

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ function destructivelyAppendCat(name){
 }
 
 
-function destructivelyprependCat(name){
+function destructivelyPrependCat(name){
     cats.unshift("Bob");
 }
 
 
-function destructivelyRemovedLastCat(){
-    cats.pop([2]);
+function destructivelyRemoveLastCat(name){
+    cats.pop("Garfield");
 }
 
 
 
-function destructivelyRemovedFirstCat(){
-    cats.shift([0]);
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
 }
 
 function appendCat(name){
